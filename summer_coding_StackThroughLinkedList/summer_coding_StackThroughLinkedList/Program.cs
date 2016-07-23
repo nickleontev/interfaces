f4d0a61867ea24eca5857630bb01b6aa64d52d49
Никита Леонтьев < >
2016-07-23 22:01:44 +0400

    Добавил проект - реализация стэка через двухсвязный список
    Ссылка на источник:https://www.youtube.com/watch?v=o14BDVs2WkQ

---
+++ summer_coding_StackThroughLinkedList/summer_coding_StackThroughLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summer_coding_StackThroughLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackThroughLinkedList<int> stack = new StackThroughLinkedList<int>();
+
+            stack.Push(33);
+            stack.Push(11);
+
+            Console.WriteLine(stack.Count);
+
+            stack.Pop();
+            
+            Console.WriteLine(stack.Peek());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ summer_coding_StackThroughLinkedList/summer_coding_StackThroughLinkedList/StackThroughLinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summer_coding_StackThroughLinkedList
+{
+    class StackThroughLinkedList <T>
+    {
+        LinkedList<T> _items = new LinkedList<T>();
+
+        public void Push(T value)
+        {
+            _items.AddLast(value);
+        }
+
+        public T Pop ()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("");
+            }
+            T resault = _items.Last.Value;
+            _items.RemoveLast();
+            return resault;
+        }
+
+        public T Peek()
+        {
+            return _items.Last.Value;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _items.Count;
+            }
+        }
+
+
+    }
+}
