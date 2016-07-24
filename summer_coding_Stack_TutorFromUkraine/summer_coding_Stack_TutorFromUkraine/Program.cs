923ec4ce44303fe972afb2d4a9e226645f43bb08
Никита Леонтьев < >
2016-07-24 13:16:42 +0400

    Добавил проект - реализация стэка через ссылки
    пс.  Работу не проверял, просто набрал пример украинского быдлокодера
    псс. Тому, кто разберется и объяснит как это делается - будет великая благодарность, т.к. я пока не понимаю
    
    Ссылка на источник: https://www.youtube.com/watch?v=k-t_SbMJEnw

---
+++ summer_coding_Stack_TutorFromUkraine/summer_coding_Stack_TutorFromUkraine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summer_coding_Stack_TutorFromUkraine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ summer_coding_Stack_TutorFromUkraine/summer_coding_Stack_TutorFromUkraine/Stack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summer_coding_Stack_TutorFromUkraine
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node()
+        {
+            this.data = 0;
+            this.next = null;
+
+        }
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+
+        }
+
+
+    }
+
+    class Stack
+    {
+        private Node begin;
+        public Stack()
+        {
+            this.begin = null;
+
+        }
+
+
+        private bool IsEmpty()
+        {
+            return this.begin == null;
+        }
+
+        public void Push(int data)
+        {
+            if (IsEmpty())
+            {
+                this.begin = new Node(data);
+            }
+            else
+            {
+                Node newNode = new Node(data);
+                newNode.next = this.begin;
+                this.begin = newNode;
+            }
+        }
+
+        public Node Begin
+        {
+            get
+            {
+                return this.begin;
+            }
+        }
+
+        public void Pop()
+        {
+            if (!IsEmpty())
+            {
+                Node tmp = this.begin;
+                this.begin = tmp.next;
+                tmp.next = null;
+            }
+
+            else
+            {
+                throw new Exception("Cтэк пуст");
+            }
+        }
+
+
+    }
+}
